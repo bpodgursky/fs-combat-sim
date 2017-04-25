c00ea83a492a99bb0061d8e2f4ba35f7702a69e6
Porter Westling <pwestling@liveramp.com>
2017-04-24 20:03:04 -0700

    add all factions basic units

--- src/main/java/com/bpodgursky/fs_combat_sim/game_state/Unit.java
+++ src/main/java/com/bpodgursky/fs_combat_sim/game_state/Unit.java
@@ -6,7 +6,10 @@ public enum Unit {
   LAND_RAIDER(3, 4, 3, 2, 4),
   WARLORD_TITAN(3, 5, 4, 3, 5),
   STRIKE_CRUISER(2, 2, 2, 0, 2),
-  BATTLE_BARGE(4, 4, 3, 2, 5);
+  BATTLE_BARGE(4, 4, 3, 2, 5),
+  ORK_BOYZ(2,2,1,0,2),
+  ASPECT_WARRIORS(2,1,2,0,2),
+  CULTIST(1,2,2,0,2);
 
   private final int dice;
   private final int health;
@@ -15,7 +18,6 @@ public enum Unit {
   private final int materialCost;
 
   Unit(int dice, int health, int morale, int commandLevel, int materialCost) {
-
     this.dice = dice;
     this.health = health;
     this.morale = morale;

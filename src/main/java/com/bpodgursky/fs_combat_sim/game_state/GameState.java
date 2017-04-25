933a755c09b3ccae278db06a5aa81ddd899e5d51
System Administrator <bpodgursky@liveramp.com>
2017-04-24 19:43:23 -0700

    stubs;

---
+++ src/main/java/com/bpodgursky/fs_combat_sim/game_state/GameState.java
@@ -0,0 +1,9 @@
+package com.bpodgursky.fs_combat_sim.game_state;
+
+import java.util.Map;
+
+public class GameState {
+
+  Map<Integer, PlayerState> playerStates;
+
+}
---
+++ src/main/java/com/bpodgursky/fs_combat_sim/game_state/PlayerState.java
@@ -0,0 +1,11 @@
+package com.bpodgursky.fs_combat_sim.game_state;
+
+public class PlayerState {
+
+  //  units
+
+  //  cities
+
+  //  cards in hand
+
+}
